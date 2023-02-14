013b153c6ec971d143cdb3c2f28f6ab58fcf8b17
NabiL Ashbat <nbl.dev21@gmail.com>
2023-02-14 14:22:36 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    const form = document.querySelector('#create-task-form');
+    let p = document.querySelector('#alert');
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const input = document.querySelector('#new-task-description');
+        const taskList = document.querySelector('#tasks');
+        const li = document.createElement('li');
+
+        if (input.value) {
+
+            li.innerHTML = input.value;
+
+            taskList.appendChild(li);
+
+            input.value = '';
+
+            if (p) {
+                p.remove();
+                p = null;
+            }
+        } else {
+            if (!p) {
+                p = document.createElement('p');
+                p.textContent = 'Enter some text please!';
+                p.id = 'alert';
+                form.insertAdjacentElement('afterend', p);
+            } else {
+                p.textContent = 'Enter some text please!';
+            }
+        }
+    });
+
+
 });
